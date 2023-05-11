27f776a4bb814e0f282b11e631ed33fdc7f69aca
almightymexijew <ctepper625@gmail.com>
2023-05-11 13:27:06 +0300

    2 swings at it

---
+++ attempt1/main.js
@@ -0,0 +1,5 @@
+const screenW = window.innerWidth;
+const screenH = window.innerHeight;
+const screensizer = `${screenW} x ${screenH}`;
+const screensizerDOM = document.getElementById('screensizer');
+screensizerDOM.value = screensizer;
---
+++ attempt2/main.js
@@ -0,0 +1,6 @@
+function screenSizer(){
+    if(window.innerWidth > 738){
+        alert("bigger than 738")
+    }else if(window.innerWidth > 412){
+        alert("bigger than 412")}
+}
